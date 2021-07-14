0922e4228e02c95e10defd9719d622b4ea4b7a43
arnoldvaz27 <arnoldvaz27.github@gmail.com>
2021-07-14 19:58:30 +0530

    Countries of Africa project added to git

---
+++ app/src/main/java/com/arnold/countriesofafrica/listeners/CountryListeners.java
@@ -0,0 +1,9 @@
+package com.arnold.countriesofafrica.listeners;
+
+import com.arnold.countriesofafrica.entites.Country;
+
+public interface CountryListeners {
+
+    void onItemClicked(Country country, int position); //event listener after clicking on the view
+
+}
